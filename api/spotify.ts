7f0f47b30672a21c2a7b285e148e28ba2fcae6dd
wcastand <wcastand@gmail.com>
2020-11-27 19:01:49 +0100

    Initial commit

--- api/spotify.ts
+++ api/spotify.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import {} from 'url'
 import { NowRequest, NowResponse } from '@vercel/node'
 
 import { getAccessToken } from '../utils'
@@ -14,22 +15,36 @@ export default async (request: NowRequest, response: NowResponse) => {
     headers: { Authorization: `Bearer ${token}` },
   }
   const info = await fetch('https://api.spotify.com/v1/me/player/currently-playing', opts)
-    .then((res) => res.json())
-    .then((json) => ({
-      artists: json.item?.artists?.map((artist) => artist?.name ?? 'Unknown').join(', '),
-      title: json.item?.name ?? 'Unknown',
-      img: json.item?.album?.images[0] ?? null,
-      year: new Date(json.item?.album?.release_date).getFullYear(),
-    }))
+    .then((res: any) => res.json())
+    .then((json: any) =>
+      fetch(json.item?.album?.images[0].url)
+        .then((res: any) => res.arrayBuffer())
+        .then((buffer: any) => {
+          const buff = Buffer.from(buffer)
+          return {
+            artists: json.item?.artists?.map((artist: { name: string }) => artist?.name ?? 'Unknown').join(', '),
+            title: json.item?.name ?? 'Unknown',
+            img: buff.toString('base64'),
+            year: new Date(json.item?.album?.release_date).getFullYear(),
+          }
+        })
+    )
     .catch(() =>
       fetch('https://api.spotify.com/v1/me/player/recently-played', opts)
-        .then((res) => res.json())
-        .then((json) => ({
-          artists: json.items[0]?.track?.artists?.map((artist) => artist?.name ?? 'Unknown').join(', '),
-          title: json.items[0]?.track?.name ?? 'Unknown',
-          img: json.items[0]?.track?.album?.images[0] ?? null,
-          year: new Date(json.items[0]?.track?.album?.release_date).getFullYear(),
-        }))
+        .then((res: any) => res.json())
+        .then((json: any) =>
+          fetch(json.items[0]?.track?.album?.images[0].url)
+            .then((res: any) => res.arrayBuffer())
+            .then((buffer: any) => {
+              const buff = Buffer.from(buffer)
+              return {
+                artists: json.items[0]?.track?.artists?.map((artist: { name: string }) => artist?.name ?? 'Unknown').join(', '),
+                title: json.items[0]?.track?.name ?? 'Unknown',
+                img: buff.toString('base64'),
+                year: new Date(json.items[0]?.track?.album?.release_date).getFullYear(),
+              }
+            })
+        )
     )
   const svg = `
     <svg viewBox="0 0 290 90" width="290" height="90" xmlns="http://www.w3.org/2000/svg">
@@ -41,7 +56,7 @@ export default async (request: NowRequest, response: NowResponse) => {
         <rect x="90" y="10" width="180" height="26"/>
       </clipPath>
       <rect x="0" y="0" fill="#171718" height="90" width="290" rx="2" />
-      <image x="13" y="13" href="${info.img.url}" height="64" width="64" preserveAspectRatio="xMidYMid meet" />
+      <image x="13" y="13" href="data:image/png;base64, ${info.img}" height="64" width="64" preserveAspectRatio="xMidYMid meet" />
       <g x="90" y="13" width="100" height="26" clip-path="url(#clip1)">
         <text x="90" y="26" class="medium">
         <title>${info.title}</title>
