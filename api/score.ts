2dcc762f30f537c13501d4cf1ab746d5ea782a0d
wcastand <wcastand@gmail.com>
2020-11-30 12:11:41 +0100

    fix

--- api/score.ts
+++ api/score.ts
@@ -5,7 +5,7 @@ import { clean, getGameState } from '../utils'
 export default async (request: NowRequest, response: NowResponse) => {
   const gamestate = await getGameState()
   const svg = `
-  <svg viewBox="0 0 100 55" width="100" height="55" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 150 75" width="150" height="75" xmlns="http://www.w3.org/2000/svg">
     <style>
       .small { font: normal 12px sans-serif; fill:#171718; }
       .bold { font: bold 14px sans-serif; fill:#171718; }
@@ -13,6 +13,7 @@ export default async (request: NowRequest, response: NowResponse) => {
     <text x="4" y="14" class="bold">Score</text>
     <text x="4" y="32" class="small">Circle: ${gamestate.circle || 0}</text>
     <text x="4" y="48" class="small">Cross: ${gamestate.cross || 0}</text>
+    <text x="4" y="64" class="small">Cross: ${gamestate.turn ? 'Cross' : 'Circle'}'s turn</text>
   </svg>
   `
 
