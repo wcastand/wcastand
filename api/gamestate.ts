f5a683e8d694ef0a98af18042d0a199c67e96e52
wcastand <wcastand@gmail.com>
2021-06-18 21:11:43 +0200

    fix gamestat player turn

--- api/gamestate.ts
+++ api/gamestate.ts
@@ -12,7 +12,7 @@ export default async (_: NowRequest, response: NowResponse) => {
   const svg = `
     <svg viewBox="0 0 130 50" width="130" height="50" xmlns="http://www.w3.org/2000/svg">
       <rect width="130" height="50" x="0" y="0" rx="6" ry="6" fill="#fff" stroke="#171718" stroke-width="2"/>
-      <text x="10" y="20" font-size="14px">Player's turn: ${playerturn === 0 ? 'Cross' : 'Circle'}</text>
+      <text x="10" y="20" font-size="14px">Player's turn: ${playerturn === 0 ? 'Circle' : 'Cross'}</text>
       <text x="10" y="38" font-size="14px">Cross: ${score[0]}</text>
       <text x="70" y="38" font-size="14px">Circle: ${score[1]}</text>
     </svg>
